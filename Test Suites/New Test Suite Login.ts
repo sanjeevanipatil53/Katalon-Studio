919d6aa917d6566a155f2f6e1f0183258c6265f0
sanjeevanipatil53 <124129645+sanjeevanipatil53@users.noreply.github.com>
2023-10-30 14:54:08 +0530

    Added test suite

---
+++ Test Suites/New Test Suite Login.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>79398a64-bde9-41ed-84d4-24770a99d41b</testSuiteGuid>
+   <testCaseLink>
+      <guid>fedd1bed-3d95-484b-bf05-5245318ca60d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
